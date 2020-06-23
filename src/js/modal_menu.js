bb325bf07e4e46cf07f608e5d719fd6085f72482
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-06-23 23:46:20 +0300

    no scril fix

--- src/js/modal_menu.js
+++ src/js/modal_menu.js
@@ -4,11 +4,13 @@ const openNavModalBtn = document.querySelector('.js-menu-button-open');
 const closeNavModalBtn = document.querySelector('.js-menu-button-close');
 const navModal = document.querySelector('.js-modal-container');
 const body = document.getElementsByTagName('body');
+// const html = document.getElementsByTagName('html');
 
 const modalHandler = () => {
   // navModal.classList.toggle("block");
   navModal.classList.toggle('modal-container-show');
   body[0].classList.toggle('no-scroll');
+  // html[0].classList.toggle('no-slip-scroll');
 };
 
 openNavModalBtn.addEventListener('click', modalHandler);
