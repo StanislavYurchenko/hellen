37670a04818392619cf4142922c38fbe48ac1d2e
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-06-24 14:26:04 +0300

    burger menu link

--- src/js/modal_menu.js
+++ src/js/modal_menu.js
@@ -2,6 +2,7 @@
 
 const openNavModalBtn = document.querySelector('.js-menu-button-open');
 const closeNavModalBtn = document.querySelector('.js-menu-button-close');
+const selectNavRef = document.querySelectorAll('.navigation-menu__item');
 const navModal = document.querySelector('.js-modal-container');
 const body = document.getElementsByTagName('body');
 
@@ -12,3 +13,7 @@ const modalHandler = () => {
 
 openNavModalBtn.addEventListener('click', modalHandler);
 closeNavModalBtn.addEventListener('click', modalHandler);
+
+for (let i = 0; i < selectNavRef.length; i++) {
+  selectNavRef[i].addEventListener('click', modalHandler);
+}
