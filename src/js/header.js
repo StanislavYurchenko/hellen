68052bcfb3bcb98325c996221db2b1fed13dc20a
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-06-25 00:31:32 +0300

    top menu buttom

--- src/js/header.js
+++ src/js/header.js
@@ -1,3 +1,24 @@
-const buttonFreeClassRef = document.querySelector('#FreeClassButton');
+'use strict';
 
-buttonFreeClassRef.addEventListener('click', () => {});
+const buttonFreeClassRef = document.querySelector('#FreeClassButton');
+const modalConsRef = document.querySelector('.consultation');
+const modalConsBgRef = document.querySelector('.consultation__background');
+const modalConsBnCloseRef = document.querySelector(
+  '.consultation__button-close',
+);
+const modalConsBoxRef = document.querySelector('.consultation__box-heading');
+
+const consultationHolder = () => {
+  modalConsRef.classList.toggle('consultation--modal-show');
+  modalConsBgRef.classList.toggle('consultation__background--modal');
+  modalConsBnCloseRef.classList.toggle('consultation__button-close--visible');
+  modalConsBoxRef.classList.toggle('display-none');
+};
+
+buttonFreeClassRef.addEventListener('click', () => {
+  consultationHolder();
+});
+
+modalConsBnCloseRef.addEventListener('click', () => {
+  consultationHolder();
+});
