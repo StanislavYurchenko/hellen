83452bbdbf9d0cb678c0844ac990b79f72008c07
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-06-25 07:41:09 +0300

    ready

--- src/js/header.js
+++ src/js/header.js
@@ -7,12 +7,14 @@ const modalConsBnCloseRef = document.querySelector(
   '.consultation__button-close',
 );
 const modalConsBoxRef = document.querySelector('.consultation__box-heading');
+const body = document.getElementsByTagName('body');
 
 const consultationHolder = () => {
   modalConsRef.classList.toggle('consultation--modal-show');
   modalConsBgRef.classList.toggle('consultation__background--modal');
   modalConsBnCloseRef.classList.toggle('consultation__button-close--visible');
   modalConsBoxRef.classList.toggle('display-none');
+  body[0].classList.toggle('no-scroll');
 };
 
 buttonFreeClassRef.addEventListener('click', () => {
